3a0322055eb177a8231a37aa10e1318cc74662b6
HitzSPB <daniel.oesterbro@gmail.com>
2021-06-11 18:53:49 +0200

    Added more steps of the game

--- js/Game.js
+++ js/Game.js
@@ -11,10 +11,21 @@ class Game {
     }
     createPhrases()
     {
-        return [ {phrase:"This is a game"},{phrase:"TeamTreeHouse JS exam"}, {phrase:"We will make it"}, {phrase:"This is a test app"},{phrase:"Game show app"}]
+        return [ new Phrase("This is a game"),new Phrase("TeamTreeHouse JS exam"), new Phrase("We will make it"), new Phrase("This is a test app"),new Phrase("Game show app")]
+        // return [ {phrase:"This is a game"},{phrase:"TeamTreeHouse JS exam"}, {phrase:"We will make it"}, {phrase:"This is a test app"},{phrase:"Game show app"}]
     }
     getRandomPhrase() {
-        const randomPhrase = Math.floor(Math.random() * this.phrases.length);
-        return this.phrases[randomPhrase];
+        const getRandomNubmerWithinRange = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[getRandomNubmerWithinRange];
     };
+    startGame(){
+        document.querySelector("#overlay").remove();
+        this.createPhrases();
+        this.getRandomPhrase();
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addhPraseToDisplay();
+    };
+    handleInteraction() {
+
+    }
   }
