0b920c35e25525d5b1364f8b38a0525ad7b9303f
HitzSPB <daniel.oesterbro@gmail.com>
2021-06-11 16:16:09 +0200

    Added first few basic game logic steps

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor()
+    {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+    createPhrases()
+    {
+        return [ {phrase:"This is a game"},{phrase:"TeamTreeHouse JS exam"}, {phrase:"We will make it"}, {phrase:"This is a test app"},{phrase:"Game show app"}]
+    }
+    getRandomPhrase() {
+        const randomPhrase = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomPhrase];
+    };
+  }
