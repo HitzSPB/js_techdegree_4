d44bb19e3bcee70f00aca08314854348acf35728
HitzSPB <daniel.oesterbro@gmail.com>
2021-06-12 10:47:53 +0200

    Methods adding to make the game work

--- js/Game.js
+++ js/Game.js
@@ -11,21 +11,75 @@ class Game {
     }
     createPhrases()
     {
-        return [ new Phrase("This is a game"),new Phrase("TeamTreeHouse JS exam"), new Phrase("We will make it"), new Phrase("This is a test app"),new Phrase("Game show app")]
-        // return [ {phrase:"This is a game"},{phrase:"TeamTreeHouse JS exam"}, {phrase:"We will make it"}, {phrase:"This is a test app"},{phrase:"Game show app"}]
-    }
+        return [ new Phrase("This is a game"), new Phrase("TeamTreeHouse JS exam"), new Phrase("We will make it"), new Phrase("This is a test app"), new Phrase("Game show app")]
+    };
+
     getRandomPhrase() {
         const getRandomNubmerWithinRange = Math.floor(Math.random() * this.phrases.length);
         return this.phrases[getRandomNubmerWithinRange];
     };
+
     startGame(){
-        document.querySelector("#overlay").remove();
-        this.createPhrases();
+        document.querySelector("#overlay").style.display = "none";
         this.getRandomPhrase();
         this.activePhrase = this.getRandomPhrase();
         this.activePhrase.addhPraseToDisplay();
     };
-    handleInteraction() {
 
+    handleInteraction(character) {
+        character.disabled = true;
+        if(this.activePhrase.checkLetter(character.textContent)){
+            character.classList.add("chosen");
+            this.activePhrase.showMatchedLetter(character.textContent);
+            if(this.checkForWin()){
+                this.gameOver(true);
+            }
+        }
+        else {
+            character.classList.add("wrong");
+            this.removeLife();
+
+        }
     }
+    checkForWin(){
+  // checking if <li> elements have a class 'hide letter'
+        if (document.querySelectorAll("#phrase li.hide").length !== 0) {
+            return false;
+        } 
+        else {
+            return true;
+        }
+    }
+    removeLife() {
+        let lifeImages = Array.from(document.querySelectorAll('img')).reverse();
+        lifeImages[this.missed].src="images/lostHeart.png"
+        this.missed += 1;
+        if (this.missed === 5 ){
+            this.gameOver(false);
+        }
+    }
+    gameOver(victory){
+        let overlay = document.querySelector("#overlay");
+        let gameMessage = document.querySelector("#game-over-message");
+        overlay.style.display = "";
+        if (victory) {
+            overlay.classList.add("win");   
+            gameMessage.textContent = "Eyyy you won the game";
+        } else {
+            overlay.className = "lose";
+            gameMessage.textContent = "Not everyone is a winner";
+        }
+        this.resetGame();
+    }
+
+    resetGame(){
+        this.missed = 0;
+        document.querySelector('#phrase ul').innerHTML = "";
+        document.querySelectorAll('img').forEach(x => x.src = "images/liveHeart.png")
+        document.querySelectorAll('#qwerty button').forEach(x => {
+                x.disabled = false;
+                x.className = "key";
+            }
+        );
+    };
   }
--- js/app.js
+++ js/app.js
@@ -6,10 +6,15 @@ const game = new Game();
 
 document.querySelector("#btn__reset").addEventListener("click", function()
 {
-game.startGame();
+ game.startGame();
 });
 
 
-
+document.getElementById('qwerty').addEventListener('click',(event)=>{
+    console.log(event.target);
+    if (event.target.className==='key'){
+        game.handleInteraction(event.target);
+    }
+});
 
 // game.startGame();
